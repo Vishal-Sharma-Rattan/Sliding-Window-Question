aa7527c0fd31bb9e2e43724d38659b2b22e7e7e3
Vishal-Sharma-Rattan <122700707+Vishal-Sharma-Rattan@users.noreply.github.com>
2023-01-22 00:03:10 +0530

    Update Sliding_Window_Maximum.cpp

--- Sliding_Window_Maximum.cpp
+++ Sliding_Window_Maximum.cpp
@@ -1,3 +1,7 @@
+// code link:
+//  https://leetcode.com/problems/sliding-window-maximum/description/
+
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& a, int k) {
@@ -30,3 +34,45 @@ public:
         return v;
     }
 };
+/*
+// Approach 2 : using queue
+#include<iostream>
+#include<bits/stdc++.h>
+#include "binaryTreeNode.h"
+using namespace std;
+
+
+vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+    deque<int>q;
+        int i=0,j=0;
+        vector<int>res;
+        while(j<arr.size()){
+            while(!q.empty() && q.back()<arr[j]) q.pop_back();
+            q.push_back(arr[j]);
+            if(j-i+1<k) j++;
+            else if(j-i+1==k){
+                res.push_back(q.front());
+                if(q.front()==arr[i]) q.pop_front();
+                i++;
+                j++;
+            }
+        }
+        return res;
+    
+}
+int main()
+{
+    vector<int>v;
+    int n; cin >> n;
+    for(int i = 0; i< n; i++ ){
+        int t; cin >> t;
+        v.push_back(t);
+    }
+    int k ; cin >> k;
+    vector<int>v1 = maxSlidingWindow(v,k);
+    for(auto i : v1)
+    {
+        cout << i <<" ";//[3,3,5,5,6,7]
+    }
+}
+*/
